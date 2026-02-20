13f615e7426232a9e8d83bd2a0cda213e032221b
Kushal Rathod <kushalrathod044@gmail.com>
2026-02-20 08:51:55 +0530

    add: flowui cli registry

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -9,27 +9,6 @@ export default function Layout({ children }: LayoutProps<'/docs'>) {
       {...baseOptions()}
       sidebar={{
         className: "w-[270px] shrink-0 bg-background border-r font-poppins text-[15px]",
-        tabs: {
-          transform(option, node) {
-            const meta = source.getNodeMeta(node);
-            if (!meta || !node.icon) return option;
-            return {
-              ...option,
-              icon: (
-                <div
-                  className="[&_svg]:size-full rounded-lg size-full text-(--tab-color) max-md:bg-(--tab-color)/10 max-md:border max-md:p-1.5"
-                  style={
-                    {
-                      '--tab-color': `rgb(var(--color-fd-primary))`,
-                    } as React.CSSProperties
-                  }
-                >
-                  {node.icon}
-                </div>
-              ),
-            };
-          },
-        },
       }}
       githubUrl='https://github.com/kushalxcoder/flowui'
     >
