6f13f8a67c99e27f7b901d7f711a2152bf8e2ae3
Kushal Rathod <kushalrathod044@gmail.com>
2026-03-03 20:13:07 +0530

    fix: tweets ui

--- components/app-components/landing-page/landing-page.tsx
+++ components/app-components/landing-page/landing-page.tsx
@@ -39,6 +39,12 @@ const LandingPage = () => {
             <div className="px-10 py-5">
                 <h1 className="font-caveat text-4xl">What devs say ?</h1>
                 <Tweets />
+                <div className="h-20 flex flex-1 justify-center items-center rounded-lg relative mt-5">
+                    <div className="absolute inset-0 bg-[repeating-linear-gradient(45deg,currentColor_0px,currentColor_1px,transparent_1px,transparent_12px)] opacity-[0.05] dark:opacity-[0.1]" />
+                    <h1 className="font-mono text-sm text-gray-500 text-center z-10">
+                        Waiting for more feedbacks :)
+                    </h1>
+                </div>
             </div>
 
             <Divider type="screen" />
