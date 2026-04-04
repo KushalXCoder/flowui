e9d174406ea54c3a6d4ea66057e409e7ca008274
Kushal Rathod <kushalrathod044@gmail.com>
2026-04-04 13:09:54 +0530

    fix: cleanup

--- lib/actions/read-file.ts
+++ lib/actions/read-file.ts
@@ -5,7 +5,6 @@ import fs from "node:fs";
 export const readFileAction = async (file: string) => {
     try {
         const data = fs.readFileSync(file, 'utf-8');
-        console.log(data);
         return data;
     } catch (error) {
         console.error(error);
